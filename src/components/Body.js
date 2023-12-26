aeb6a49c742797aa4d729d307abe8edd8b40064a
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-26 01:58:12 -0500

    Added body component
    Added body component

---
+++ src/components/Body.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import SidePanel from './SidePanel'
+import MainContainer from './MainContainer'
+
+const Body = () => {
+  return (
+    <div>
+      Body
+      <SidePanel />
+      <MainContainer />
+    </div>
+  )
+}
+
+export default Body
