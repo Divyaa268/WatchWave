84cc2a0aa872c4f82063f6ece9ad8bc53dcc9bd6
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-26 02:16:31 -0500

    Added logo and hamburger menu icon
    Added logo and hamburger menu icon

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Design
 - Header
+  - Hamburger Icon
+  - Logo
+  - Search bar
+  - login
+  - LogOut
 - Body
   - Side Panel
   - Menu Items
 - Main Container
   - ButtonList
-  - Video Container
+  - Video Container 
     - Video Card
-    
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      header
+    <div className='flex'>
+      <img className='h-12 w-12 mr-8' alt="menu" src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png'/>
+    
+      <img className='h-12 w-32' alt="logo" src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/YouTube_Logo_%282013-2017%29.svg/512px-YouTube_Logo_%282013-2017%29.svg.png' />
+    
     </div>
+
+
   )
 }
 
