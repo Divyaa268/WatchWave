ee9720df77e93e4d7d275007a47d5ca44155c1c2
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-29 04:48:20 -0500

    Created a side panel with options
    Created a side panel with options

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -2,8 +2,38 @@ import React from 'react'
 
 const SidePanel = () => {
   return (
-    <div>
-        SidePanel
+    <div className='p-5 shadow-lg w-48'>
+        
+        <ul>
+          <li className='flex'>
+            <img className='h-6 w-6' alt='home' src='https://static.thenounproject.com/png/3574480-200.png' /> 
+            Home</li>
+          <li className='flex'>
+          <img className='h-6 w-6' alt='home' src='https://static.thenounproject.com/png/4512655-200.png' /> 
+            Shorts</li>
+          <li  className='flex'>
+          <img className='h-6 w-6' alt='home' src='https://icon2.cleanpng.com/20180203/lee/kisspng-video-ico-icon-video-icon-transparent-background-5a75f609b0ca41.3603646815176801377241.jpg' /> 
+            Videos</li>
+          <li className='flex'>
+          <img className='h-6 w-6' alt='home' src='https://static.thenounproject.com/png/2146857-200.png' />
+            Live</li>
+        </ul>
+
+        <h1 className='font-bold pt-5'>Subscription</h1>
+        <ul>
+          <li>Music</li>
+          <li>Sports</li>
+          <li>Gaming</li>
+          <li>Movies</li>
+        </ul>
+
+        <h1 className='font-bold pt-5'>Watch Later</h1>
+        <ul>
+          <li>Music</li>
+          <li>Sports</li>
+          <li>Gaming</li>
+          <li>Movies</li>
+        </ul>
       
     </div>
   )
