657dacd7002660bc9c697655605d655cd6c36ee5
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-29 04:20:41 -0500

    styled search bar
    styled search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,21 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='grid grid-flow-col p-2 m-2 shadow-lg '>
-    <div className='flex'>
+    <div className='grid grid-flow-col p-4 m-2 shadow-lg '>
+    <div className='flex col-span-2'>
       <img className='h-12 w-12 mr-8' alt="menu" src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png'/>
     
       <img className='h-12 w-32' alt="logo" src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/YouTube_Logo_%282013-2017%29.svg/512px-YouTube_Logo_%282013-2017%29.svg.png' />
     
     </div>
 
-    <div>
-        <input className='ml-12 w-72' type='text' placeholder='what would you like to watch today?'></input>
-        <button className='bg-red-600 rounded-lg p-3'>Search</button>
+    <div className='col-span-10'>
+        <input className='ml-72 w-96 h-12 border border-gray-400 rounded-l-full text-center hover:border-gray-800' type='text' placeholder='what would you like to watch today?'></input>
+        <button className='bg-red-600 rounded-r-full p-3 h-12 border border-red-600 hover:bg-red-500'>Search</button>
 
     </div>
 
-    <div>
+    <div className='col-span-1'>
         <img className='h-12 w-12 ml-12' alt='user-icon' src='https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png' />
     </div>
     </div>
