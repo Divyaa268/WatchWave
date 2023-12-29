7250cfe0446201656f4ccdc8725e0fc46a086ec1
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-29 17:23:17 -0500

    Subscribed to specific action from store
    Subscribed to specific action from store

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const SidePanel = () => {
+
+  const isMenuOpen = useSelector(store => store.app.isMenuOpen);
+
+  // early return pattern
+  if(!isMenuOpen) return null;
+
   return (
     <div className='p-5 shadow-lg w-48'>
         
