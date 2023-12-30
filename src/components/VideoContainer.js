cb97f95310d3465819e71f4c6080fcdbd69d2098
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-30 00:09:22 -0500

    Fetched Youtube data from API
    Fetched Youtube data from API

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { YOUTUBE_POPULAR_API } from '../utils/constants';
+import VideoCard from './VideoCard';
 
 const VideoContainer = () => {
+
+  const [ videos, setVideos ] = useState([]);
+  
+  useEffect(() => {
+    getPopularVideos();
+  }
+  , []);
+
+  const getPopularVideos = async () => {
+    const data = await fetch(YOUTUBE_POPULAR_API);
+    const json =  await data.json();
+
+    console.log(json.items);
+
+    setVideos(json.items);
+  }
+
   return (
-    <div>
-      VideoContainer
+    <div className="flex flex-wrap">
+      {videos.map(video => (
+      <VideoCard key={video.id} info={video} />
+      ))}
+      {/* <VideoCard info = { videos[0]} /> */}
     </div>
   )
 }
