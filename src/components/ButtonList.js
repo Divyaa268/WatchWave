986383adebfa9a20f5fbdb1e14d4ff644b64cdf4
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-29 22:24:37 -0500

    Created a button list
    Created a button list

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Button from './Button'
 
 const ButtonList = () => {
   return (
-    <div>
-      buttonlist
+    <div className='flex'>
+      <Button name="All"/>
+      <Button name="Gaming"/>
+      <Button name="Music"/>
+      <Button name="Live"/>
+      <Button name="Soccer"/>
+      <Button name="Cricket"/>
+      <Button name="Cooking"/>
+      <Button name="Trailers"/>
+      <Button name="Formula 1"/>
+
     </div>
   )
 }
