46725adaa06f6ac45b28133f0e936d6bfedb8c65
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-30 00:09:52 -0500

    Displayed Video Card with details
    Displayed Video Card with details

---
+++ src/components/VideoCard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const VideoCard = ({info}) => {
+    console.log(info);
+
+    const { snippet, statistics } = info;
+
+    const { channelTitle, description, title, thumbnails, publishedAt } = snippet;
+
+  return (
+    <div className='p-2 m-2 w-72 shadow-lg cursor-pointer'>
+        <img className='rounded-lg' alt='thumbnail' src = {thumbnails.medium.url} />
+        <ul>
+            <li className='font-bold py-2 text-wrap'>{ title }</li>
+            <li>{ channelTitle }</li>
+            <li>{ statistics.viewCount } views</li>
+            {/* <li>{ publishedAt }</li> */}
+        </ul>
+      
+    </div>
+  )
+}
+
+export default VideoCard;
