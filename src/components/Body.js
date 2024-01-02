f0be0b520d68ce7b5a6c9fd8e248ec2b12cc4470
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-01-02 02:16:17 -0500

    Added Outlet
    Added Outlet

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import SidePanel from './SidePanel'
 import MainContainer from './MainContainer'
+import { Outlet } from 'react-router-dom'
 
 const Body = () => {
   return (
     <div className='grid grid-flow-col'>
       
       <SidePanel />
-      <MainContainer />
+      {/* <MainContainer /> */}
+      <Outlet />
     </div>
   )
 }
