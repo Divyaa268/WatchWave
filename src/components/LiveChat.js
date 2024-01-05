003e65859ad0c4d52ba815b6b2d1aef568ad1992
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-01-05 04:09:25 -0500

    Built chat message and live chat components
    Built chat message and live chat components

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import ChatMessage from './ChatMessage'
  
 const LiveChat = () => {
   return (
     <div className='p-2 m-2 w-full h-[590px] border border-black bg-slate-100 rounded-lg'>
-        Live Chat
+       <h3 className='font-bold py-2 px-2'>Live Chat</h3> 
+
+        <ChatMessage 
+            name="Divya"
+            message="Working on Namaste React!🙏" />
       
     </div>
   )
