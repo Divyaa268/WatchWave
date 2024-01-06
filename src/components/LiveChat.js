906a79f067f316b6698b133e6d5d0847869bc51b
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-01-05 20:24:07 -0500

    added input box for sending text
    added input box for sending text

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -27,22 +27,35 @@ const LiveChat = () => {
 
   return (
     
-    <div className='p-2 m-2 w-full h-[590px] border border-black bg-slate-100 rounded-lg overflow-y-scroll shadow-md flex flex-col-reverse'>
+    // <div className='p-2 m-2 w-full h-[590px] border border-black bg-slate-100 rounded-lg overflow-y-scroll overflow-hidden shadow-md flex flex-col-reverse'>
+    //     <h3 className='font-bold py-2 px-2'>Live Chat</h3> 
+    //     <div className=''>
+
+    <>
+    <div className='p-2 m-2 w-full h-[590px] border border-black bg-slate-100 rounded-lg overflow-y-scroll overflow-hidden shadow-md'>
         <h3 className='font-bold py-2 px-2'>Live Chat</h3> 
-        <div className=''>
-        {/* <ChatMessage 
-            name="Divya"
-            message="Working on Namaste React!🙏" /> */}
+        <div className=' flex flex-col-reverse'> 
+        <div>
 
             {chatMessages.map((m, index) => (
                 <ChatMessage key={index}
                 name={m.name}
                 message={m.message} />
             ))}
+        
+        </div>
+        </div>
+    </div>
+
+        <div className='w-full p-2 ml-2 border border-slate-300 rounded-lg'>
+            <input className='w-96' type='text'>
+            </input>
+                <button className='px-3 py-2 m-2 bg-slate-300 font-semibold rounded-lg hover:bg-slate-100'> Send </button>
+            
 
         </div>
       
-    </div>
+    </>
   )
 }
 
